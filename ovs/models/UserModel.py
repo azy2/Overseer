2a2614ad54b3250be35e5e377d44226d43548bb0
Tan, Joe <ztan19@illinois.edu>
2018-02-18 20:52:36 +0000

    Merged PR 10: implemented user authentication
    implemented user authentication
    
    Related work items: #23, #50, #53, #62

--- ovs/models/UserModel.py
+++ ovs/models/UserModel.py
@@ -1,9 +1,11 @@
-from flask import jsonify
-import sqlalchemy as sa
-from ovs import app
 import bcrypt
+import sqlalchemy as sa
+from flask import jsonify
+
+from ovs import app
 
 SALT_ROUNDS = 12
+
 
 class User(app.BaseModel):
     __tablename__ = 'users'
@@ -13,12 +15,16 @@ class User(app.BaseModel):
     first_name = sa.Column(sa.String(255), nullable=False)
     last_name = sa.Column(sa.String(255), nullable=False)
     password = sa.Column(sa.CHAR(60), nullable=False)
-    role = sa.Column(sa.Enum('RESIDENT', 'RESIDENT_ADVISOR', 'STAFF', 'OFFICE_MANAGER', 'BUILDING_MANAGER', 'ADMIN'), nullable=False)
-    created = sa.Column(sa.DateTime, server_default=sa.text('CURRENT_TIMESTAMP'))
-    updated = sa.Column(sa.DateTime, server_default=sa.text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+    role = sa.Column(sa.Enum('RESIDENT', 'RESIDENT_ADVISOR', 'STAFF',
+                             'OFFICE_MANAGER', 'BUILDING_MANAGER', 'ADMIN'), nullable=False)
+    created = sa.Column(
+        sa.DateTime, server_default=sa.text('CURRENT_TIMESTAMP'))
+    updated = sa.Column(sa.DateTime, server_default=sa.text(
+        'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
 
     def __init__(self, email, first_name, last_name, password, role):
-        password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(SALT_ROUNDS))
+        password = bcrypt.hashpw(password.encode(
+            'utf-8'), bcrypt.gensalt(SALT_ROUNDS))
         super(User, self).__init__(
             email=email,
             password=password,
@@ -28,18 +34,31 @@ class User(app.BaseModel):
 
     def __repr__(self):
         return "User([id='%s', email='%s', first_name='%s', last_name='%s', role='%s', created='%s', updated='%s'])" % \
-            (self.id, self.email, self.first_name, self.last_name, self.role, self.created, self.updated)
+            (self.id, self.email, self.first_name, self.last_name,
+             self.role, self.created, self.updated)
 
     def has_password(self, password):
         return self.password == bcrypt.hashpw(password, self.password)
 
     def json(self):
         return jsonify(
-            id = self.id,
-            email = self.email,
-            first_name = self.first_name,
-            last_name = self.last_name,
-            role = self.role,
-            created = self.created,
-            updated = self.updated
+            id=self.id,
+            email=self.email,
+            first_name=self.first_name,
+            last_name=self.last_name,
+            role=self.role,
+            created=self.created,
+            updated=self.updated
         )
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
--- ovs/routes/__init__.py
+++ ovs/routes/__init__.py
@@ -1,3 +1,4 @@
+from .AuthRoutes import auth_bp as AuthRoutes
 from .UserRoutes import users_bp as UserRoutes
 from .AdminRoutes import admin_bp as AdminRoutes
 from .DevRoutes import dev_home_page_bp as DevRoutes
