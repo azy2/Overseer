db1cfa39edadbeb914a699429a25d949da5e9247
Ben Lambeth <lambethben@gmail.com>
2018-02-20 17:57:36 -0600

    Fixed non-determinism bug in test_create_user

--- ovs/tests/test_user_service.py
+++ ovs/tests/test_user_service.py
@@ -22,11 +22,11 @@ class TestUserService(TestCase):
 
     def test_create_user(self):
         """ Tests create_user """
-        test_user = {'email': 'test@gmail.com', 'first_name': 'Bob',
-                     'last_name': 'Ross', 'role': 'ADMIN'}
-        UserService.create_user(**test_user)
-        user_list = self.db.query(User).filter(User.email == test_user['email']).all()
+        test_user_info = ('test@gmail.com', 'Bob', 'Ross', 'ADMIN')
+        UserService.create_user(*test_user_info)
+        user_list = self.db.query(User).filter(User.email == test_user_info[0]).all()
         self.assertEqual(len(user_list), 1)
         user = user_list[0]
-        self.assertEqual((user.email, user.first_name, user.last_name, user.role), tuple(test_user.values()))
+        self.assertEqual((user.email, user.first_name,
+                          user.last_name, user.role), test_user_info)
         self.assertIsNotNone(user.password)
