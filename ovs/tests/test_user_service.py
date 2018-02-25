c0f3dc1ea552c78905c32baf2d39a67d32790b05
David <buksa2@illinois.edu>
2018-02-25 14:54:49 -0600

    Fleshed out user_service tests

--- ovs/tests/test_user_service.py
+++ ovs/tests/test_user_service.py
@@ -31,3 +31,19 @@ class TestUserService(TestCase):
         self.assertEqual((user.email, user.first_name,
                           user.last_name, user.role), test_user_info)
         self.assertIsNotNone(user.password)
+
+    def test_get_user_by_email(self):
+        """ Tests get_user_by_email """
+        test_user_info = ('test@gmail.com', 'Bob', 'Ross', 'ADMIN')
+        UserService.create_user(*test_user_info)
+        user_list = UserService.get_user_by_email(test_user_info[0]).all()
+        self.assertEqual(len(user_list), 1)
+        user = user_list[0]
+        self.assertEqual((user.email, user.first_name,
+                          user.last_name, user.role), test_user_info)
+        self.assertIsNotNone(user.password)
+
+    def test_get_user_by_email_0(self):
+        """ Tests get_user_by_email with bad parameter"""
+        user_list = UserService.get_user_by_email("dummy@gmail.com").all()
+        self.assertEqual(len(user_list), 0)
