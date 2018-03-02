d453782ba5198bbe962c2b532bef06c0a8c43e90
AlexMackowiak <Alex@Mackowiaks.com>
2018-03-01 18:17:32 -0600

    Created profile table migration

---
+++ database/migrations/versions/851f54dc9989_create_profile_table.py
@@ -0,0 +1,31 @@
+"""create profile table
+
+Revision ID: 851f54dc9989
+Revises: 01e80453c316
+Create Date: 2018-03-01 23:51:05.631322
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '851f54dc9989'
+down_revision = '01e80453c316'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    op.create_table(
+        'profile',
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('residents.user_id'), nullable=False),
+        sa.Column('preferred_name', sa.CHAR(255)),
+        sa.Column('phone_number', sa.CHAR(31)),
+        sa.Column('preferred_email', sa.CHAR(255)),
+        sa.Column('race', sa.CHAR(31)),
+        sa.Column('picture_path', sa.CHAR(255)),
+        sa.Column('created', sa.DateTime, server_default=sa.text('CURRENT_TIMESTAMP')),
+        sa.Column('updated', sa.DateTime, server_default=sa.text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')),
+    )
+
+def downgrade():
+    op.drop_table('profile')
