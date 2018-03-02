563fe53fe8fe955cd47e6f86b1f8dd905ce03a1f
AlexMackowiak <Alex@Mackowiaks.com>
2018-03-01 19:52:57 -0600

    Initial profile model created

---
+++ ovs/models/profile_model.py
@@ -0,0 +1,47 @@
+"""
+Defines a Profile as represented in the database
+"""
+from flask import jsonify
+import sqlalchemy as sa
+from ovs import app
+
+
+class Profile(app.BaseModel):
+    """
+    Defines a Profile as represented in the database.
+    """
+    __tablename__ = 'profile'
+
+    user_id = sa.Column(sa.Integer, sa.ForeignKey('residents.user_id'), nullable=False)
+    preferred_name = sa.Column(sa.CHAR(255))
+    phone_number = sa.Column(sa.CHAR(255))
+    preferred_email = sa.Column(sa.CHAR(255))
+    race = sa.Column(sa.CHAR(31))
+    picture_path = sa.Column(sa.CHAR(255))
+    created = sa.Column(sa.DateTime, server_default=sa.text('CURRENT_TIMESTAMP'))
+    updated = sa.Column(sa.DateTime, server_default=sa.text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+
+    def __init__(self, user_id, room_number):
+        super(Profile, self).__init__(user_id=user_id)
+
+    def __repr__(self):
+        return "Profile([user_id='%s', preferred_name='%s', phone_number='%s', preferred_email='%s', race='%s', \
+                 picture_path='%s', created='%s', updated='%s'])" \
+               % (self.user_id, self.preferred_name, self.phone_number, self.preferred_email, self.picture_path, \ 
+                    self.created, self.updated)
+
+    def json(self):
+        """ Returns a JSON representation of this Profile """
+        return jsonify(
+            user_id=self.user_id,
+            preferred_name=self.preferred_name,
+            phone_number=self.phone_number,
+            preferred_email=self.preferred_email,
+            race=self.race,
+            picture_path=self.picture_path,
+            created=self.created,
+            updated=self.updated
+        )
+
+
+
