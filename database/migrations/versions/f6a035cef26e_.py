fc0aa84e6786fd5fec997203a9e67885d484365c
Ben Lambeth <lambethben@gmail.com>
2018-03-03 16:59:47 -0600

    Merge alembic heads

---
+++ database/migrations/versions/f6a035cef26e_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: f6a035cef26e
+Revises: 851f54dc9989, a0b75cd46696
+Create Date: 2018-03-03 16:55:58.172618
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f6a035cef26e'
+down_revision = ('851f54dc9989', 'a0b75cd46696')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
