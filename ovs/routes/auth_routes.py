eb4e8fdd58c6bed4ac58ab94814c24abd0c1d7ae
cpjhnsn2 <cpjhnsn2@illinois.edu>
2018-03-11 16:20:50 -0500

    Added notification modal func to base html

--- ovs/routes/auth_routes.py
+++ ovs/routes/auth_routes.py
@@ -20,10 +20,9 @@ def login():
         # password = form.password.data
         user = db.query(User).filter_by(email=email).first()
         if user is None:
-            flash('Invalid Credentials.', 'error')
+            flash('Invalid Credentials!', 'error')
             return redirect(url_for('/.landing_page'))
         login_user(user)
-        flash('success!', 'message')
         if user.role == UserRole.RESIDENT:
             return redirect(url_for('resident.landing_page'))
         else:
