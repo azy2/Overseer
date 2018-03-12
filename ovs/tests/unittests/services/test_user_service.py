d17add72e0b3d472c99f0837b672a08b70fcac7b
AlexMackowiak <Alex@Mackowiaks.com>
2018-03-12 17:57:02 -0500

    Made user service tests more descriptive

--- ovs/tests/unittests/services/test_user_service.py
+++ ovs/tests/unittests/services/test_user_service.py
@@ -22,28 +22,30 @@ class TestUserService(TestCase):
         self.db.commit()
 
     def test_create_user(self):
-        """ Tests create_user """
+        """ Tests that users can be created """
         test_user_info = ('test@gmail.com', 'Bob', 'Ross', 'ADMIN')
         UserService.create_user(*test_user_info)
+
         user_list = self.db.query(User).filter(User.email == test_user_info[0]).all()
         self.assertEqual(len(user_list), 1)
+
         user = user_list[0]
-        self.assertEqual((user.email, user.first_name,
-                          user.last_name, user.role), test_user_info)
+        self.assertEqual((user.email, user.first_name, user.last_name, user.role), test_user_info)
         self.assertIsNotNone(user.password)
 
     def test_get_user_by_email(self):
-        """ Tests get_user_by_email """
+        """ Tests get_user_by_email successfully finds a user """
         test_user_info = ('test@gmail.com', 'Bob', 'Ross', 'ADMIN')
         UserService.create_user(*test_user_info)
+
         user_list = UserService.get_user_by_email(test_user_info[0]).all()
         self.assertEqual(len(user_list), 1)
+
         user = user_list[0]
-        self.assertEqual((user.email, user.first_name,
-                          user.last_name, user.role), test_user_info)
+        self.assertEqual((user.email, user.first_name, user.last_name, user.role), test_user_info)
         self.assertIsNotNone(user.password)
 
-    def test_get_user_by_email_0(self):
-        """ Tests get_user_by_email with bad parameter"""
+    def test_invalid_get_user_by_email(self):
+        """ Tests get_user_by_email does not find anyone with an invalid email """
         user_list = UserService.get_user_by_email("dummy@gmail.com").all()
         self.assertEqual(len(user_list), 0)
