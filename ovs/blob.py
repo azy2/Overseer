22cd335862e952a1000e9cfac5971246b542cc9c
Albert-Li1 <albertl3@illinois.edu>
2018-03-12 02:27:05 -0500

    Finished most of blob service container

--- ovs/blob.py
+++ ovs/blob.py
@@ -7,6 +7,7 @@ class Blob():
     """
     Blob is the implementation for azure blob storage
     """
+    PROFILE_PICTURE_CONTAINER = 'profile_picture'
     def __init__(self, app):
         blob_config = app.config['BLOB']
         self._service = BlockBlobService(account_name = blob_config['account'],
--- ovs/services/profile_picture_service.py
+++ ovs/services/profile_picture_service.py
@@ -1,6 +1,7 @@
 """ Services related to profile pictures """
 from ovs import app
-profile_container = app.blob.instance()
+blob_instance = app.blob.instance()
+container_name = app.blob.PROFILE_PICTURE_CONTAINER
 
 
 class ProfilePictureService:
@@ -8,16 +9,33 @@ class ProfilePictureService:
 
     @staticmethod
     def create_profile_picture(picture_id, picture):
-    """ Creates a blob object in the profile picture container with the associated id """
+    """
+    Creates a blob object in the profile picture container with the associated id
+    :param picture_id: UID of picture - needs to be unique
+    :param picture: array of bytes
+    """
+    blob_instance.create_blob_from_bytes(container_name, picture_id, picture)
 
     @staticmethod
     def update_profile_picture(picture_id, picture):
-    """ Updates a blob object in the profile picture container with the associated id """
+    """ 
+    Updates a blob object in the profile picture container with the associated id
+    :param picture_id: UID of picture - needs to be unique
+    :param picture: array of bytes
+    """
+    create_profile_picture(picture_id, picture)
 
     @staticmethod
     def delete_profile_picture(picture_id):
     """ Deletes a blob object in the profile picture container with the associated id """
+    blob_instance.delete_blob(container_name, picture_id)
 
     @staticmethod
     def get_profile_picture(picture_id):
-    """ Gets a blob object in the profile picture container with the associated id """
+    """
+    Gets a blob object in the profile picture container with the associated id
+    Returns none if the picture_id does not exist
+    """
+    if not blob_instance.exists(container_name, picture_id):
+        return none
+    return blob_instance.get_blob_to_bytes(container_name, picture_id)
