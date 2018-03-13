be68f266ea7ea7227e19d689e2b78c5ba209f39d
Ben Lambeth <lambethben@gmail.com>
2018-03-12 19:29:13 -0500

    Wired up blob storage with frontend for proper profile picture support

--- ovs/routes/resident_routes.py
+++ ovs/routes/resident_routes.py
@@ -1,11 +1,13 @@
 """ under /resident """
 import base64
+import imghdr
 
 from flask import Blueprint, redirect, render_template, request, url_for, flash
 from flask_login import current_user, login_required
 
 from ovs import app
 from ovs.forms.edit_resident_profile_form import EditResidentProfileForm
+from ovs.forms.upload_profile_picture_form import UploadProfilePictureForm
 from ovs.services.profile_service import ProfileService
 from ovs.services.resident_service import ResidentService
 from ovs.services.profile_picture_service import ProfilePictureService
@@ -36,21 +38,35 @@ def edit_profile():
     if profile is None:
         return 'Could not find profile information for user with id: ' + resident_id
 
-    form = EditResidentProfileForm(obj=profile, csrf_enabled=False)
+    profile_form = EditResidentProfileForm(obj=profile, csrf_enabled=False)
+    picture_form = UploadProfilePictureForm(csrf_enabled=False)
 
     if request.method == 'POST':
-        if form.validate():
+        if profile_form.validate():
             # Set profile data in database with non-null values from the form
             ProfileService.update_profile(resident_id,
-                                          form.preferred_email.data,
-                                          form.preferred_name.data,
-                                          form.phone_number.data,
-                                          form.race.data,
-                                          form.gender.data)
+                                          profile_form.preferred_email.data,
+                                          profile_form.preferred_name.data,
+                                          profile_form.phone_number.data,
+                                          profile_form.race.data,
+                                          profile_form.gender.data)
             flash('Profile edit successfully!', 'message')
             return redirect(url_for('resident.edit_profile'))
+        elif picture_form.validate():
+            file = request.files[picture_form.profile_picture.name]
+            if file.filename == '':
+                flash('No selected file', 'error')
+                return redirect(url_for('resident.edit_profile'))
+            if file:
+                picture_data = file.stream.read()
+                if imghdr.what(None, h=picture_data) != 'png':
+                    flash('File is not a png', 'error')
+                    return redirect(url_for('resident.edit_profile'))
+                ProfilePictureService.update_profile_picture(profile.picture_id, picture_data)
+                return redirect(url_for('resident.edit_profile'))
         else:
-            return str(form.errors)
+            return str(profile_form.errors)
     else:
         pict = base64.b64encode(ProfilePictureService.get_profile_picture(profile.picture_id)).decode()
-        return render_template('resident/profile.html', role=UserRole.RESIDENT, profile=profile, pict=pict, form=form)
+        return render_template('resident/profile.html', role=UserRole.RESIDENT, profile=profile, pict=pict,
+                               profile_form=profile_form, picture_form=picture_form)
