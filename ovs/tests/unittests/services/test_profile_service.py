13f7486ea72559cb68bd9b0add86110983131acf
David <buksa2@illinois.edu>
2018-04-03 20:29:44 -0500

    Added tests for delete profile

--- ovs/tests/unittests/services/test_profile_service.py
+++ ovs/tests/unittests/services/test_profile_service.py
@@ -1,11 +1,15 @@
 """
 Tests for profile services
 """
+from ovs import app
 from ovs.services.user_service import UserService
 from ovs.services.resident_service import ResidentService
 from ovs.services.profile_service import ProfileService
+from ovs.models.profile_model import Profile
 from ovs.utils.genders import Gender
 from ovs.tests.unittests.base_test import OVSBaseTestCase
+
+db = app.database.instance()
 
 
 class TestProfileService(OVSBaseTestCase):
@@ -44,3 +48,15 @@ class TestProfileService(OVSBaseTestCase):
         self.assertEqual(profile.race, 'Black')
         self.assertEqual(profile.gender, Gender.FEMALE)
         self.assertIsNotNone(profile.picture_id)
+
+    def test_delete_profile(self):
+        """ Tests that profiles can be deleted """
+        expected = db.query(Profile).count()-1
+        self.assertTrue(ProfileService.delete_profile(self.test_user.id)) #method returns success
+        self.assertEqual(db.query(Profile).count(), expected)
+
+    def test_delete_profile_null(self):
+        """ Tests that nothing breaks when deleting a nonexistant profile """
+        expected = db.query(Profile).count()
+        self.assertFalse(ProfileService.delete_profile(self.test_user.id + 3)) #This random id is NOT the resident
+        self.assertEqual(db.query(Profile).count(), expected)
