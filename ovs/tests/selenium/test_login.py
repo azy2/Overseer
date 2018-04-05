a59a7227dadaad611280b4116d9f13ea12f5f319
AlexMackowiak <Alex@Mackowiaks.com>
2018-04-04 21:29:30 -0500

    Refactored login tests to eliminate repeat code

--- ovs/tests/selenium/test_login.py
+++ ovs/tests/selenium/test_login.py
@@ -6,17 +6,22 @@ from ovs.tests.selenium.selenium_base_test import SeleniumBaseTestCase
 class TestLogin(SeleniumBaseTestCase):
     """ Test whether users can log in """
 
+    def login_with_credentials(self, email, password):
+        """ Logs in with the provided email and password """
+        name_box = self.browser.find_element_by_name('email')
+        name_box.send_keys(email)
+        pass_box = self.browser.find_element_by_name('password')
+        pass_box.send_keys(password)
+        pass_box.send_keys(Keys.ENTER)
+
     def test_resident_login(self):
         """ Tests whether residents can log in or not """
         self.browser.get(self.base_url)
         self.assertIn('Overseer', self.browser.title)
 
-        name_box = self.browser.find_element_by_name('email')
-        name_box.send_keys("resident@gmail.com")
-        pass_box = self.browser.find_element_by_name('password')
-        pass_box.send_keys('abcd1234')
-        pass_box.send_keys(Keys.ENTER)
+        self.login_with_credentials('resident@gmail.com', 'abcd1234')
 
+        # Should be at resident greeting page
         self.assertIn('John', self.browser.title)
 
     def test_manager_login(self):
@@ -24,23 +29,17 @@ class TestLogin(SeleniumBaseTestCase):
         self.browser.get(self.base_url)
         self.assertIn('Overseer', self.browser.title)
 
-        name_box = self.browser.find_element_by_name('email')
-        name_box.send_keys('admin@gmail.com')
-        pass_box = self.browser.find_element_by_name('password')
-        pass_box.send_keys('abcd1234')
-        pass_box.send_keys(Keys.ENTER)
+        self.login_with_credentials('admin@gmail.com', 'abcd1234')
 
+        # Should be at manager greeting page
         self.assertIn(app.config['ADMIN']['first_name'], self.browser.title)
 
     def test_bad_login(self):
-        """ Tests whether a random email  can log in or not """
+        """ Tests whether a random email can log in or not """
         self.browser.get(self.base_url)
         self.assertIn('Overseer', self.browser.title)
 
-        name_box = self.browser.find_element_by_name('email')
-        name_box.send_keys('testtesttest@gmail.com')
-        pass_box = self.browser.find_element_by_name('password')
-        pass_box.send_keys('abcd1234')
-        pass_box.send_keys(Keys.ENTER)
-
+        self.login_with_credentials('testtesttest@gmail.com', 'abcd1234')
+        
+        # Page should not have changed
         self.assertEqual('Overseer', self.browser.title)
