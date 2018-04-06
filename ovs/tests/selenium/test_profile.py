cd77a253f81352652dc00a2b9fec1d70e51a8620
AlexMackowiak <Alex@Mackowiaks.com>
2018-04-06 00:48:51 -0500

    Added invalid profile picture test

--- ovs/tests/selenium/test_profile.py
+++ ovs/tests/selenium/test_profile.py
@@ -1,4 +1,5 @@
 """ Test whether profiles can be edited """
+import os
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,11 +15,7 @@ class TestProfile(SeleniumBaseTestCase):
         text_field.clear()
         text_field.send_keys(new_text)
 
-    def test_edit_profile(self):
-        """ Tests whether all fields can be edited in a resident profile """
-        self.browser.get(self.base_url)
-        self.assertIn('Overseer', self.browser.title)
-
+    def go_to_resident_profile_page(self):
         super().login_with_credentials('resident@gmail.com', 'abcd1234')
 
         # Click on account dropdown and go to Profile link
@@ -30,13 +27,20 @@ class TestProfile(SeleniumBaseTestCase):
         # Verify page changed
         self.assertIn('Edit', self.browser.title)
 
+    def test_edit_profile(self):
+        """ Tests whether all fields can be edited in a resident profile """
+        self.browser.get(self.base_url)
+        self.assertIn('Overseer', self.browser.title)
+
+        self.go_to_resident_profile_page()
+ 
         # Change all fields
         self.set_text_field_by_id('preferred_name', 'Megatron')
         self.set_text_field_by_id('phone_number', '555-555-5555')
         self.set_text_field_by_id('preferred_email', 'Megatron@mega.tron')
         self.set_text_field_by_id('race', 'Transformer')
 
-        # Currently 'Unspecified' gender is broken so need to set to Male
+        # Set 'Unspecified' gender to 'Male'
         male_gender_option = self.browser.find_element_by_id('gender-0')
         male_gender_option.click()
 
@@ -67,3 +71,26 @@ class TestProfile(SeleniumBaseTestCase):
         self.assertEqual(self.browser.find_element_by_id('phone_number').get_attribute('value'), '555-555-5555')
         self.assertEqual(self.browser.find_element_by_id('preferred_email').get_attribute('value'), 'Megatron@mega.tron')
         self.assertEqual(self.browser.find_element_by_id('race').get_attribute('value'), 'Transformer')
+
+    def test_invalid_profile_picture(self):
+        """ Tests that non-.png files cannot be uploaded as a profile picture """
+        self.browser.get(self.base_url)
+        self.assertIn('Overseer', self.browser.title)
+
+        self.go_to_resident_profile_page()
+
+        # Choose the non-picture file to upload
+        choose_profile_picture_upload = self.browser.find_element_by_id('profile_picture')
+        choose_profile_picture_upload.send_keys(os.getcwd() + '/main.py')
+
+        upload_picture_button = self.browser.find_element_by_id('upload_picture')
+        upload_picture_button.click()
+
+        # Wait for error dialog to pop up, id='notificationModal'
+        wait = WebDriverWait(self.browser, 5)
+        wait.until(EC.visibility_of_element_located((By.ID, 'notificationModal')))
+
+        # Get error message on error dialog
+        error_p_tag = self.browser.find_element_by_id('notificationModal').find_element_by_tag_name('p')
+        error_message = error_p_tag.text
+        self.assertEqual(error_message, 'File is not a png')
