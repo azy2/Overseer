01c24e0e9aa7abab75a49e2b8fb4a7ef6b4d744b
Ben Lambeth <lambethben@gmail.com>
2018-04-07 11:51:10 -0500

    Fixed mail test bug

--- ovs/services/mail_service.py
+++ ovs/services/mail_service.py
@@ -3,15 +3,14 @@ Send Mail functions.
 """
 from flask import current_app
 
-mail = current_app.extensions['mail']
-DOMAIN_NAME = current_app.config['DOMAIN_NAME']
-
-
 class MailService:
     """Contains methods related to the sendgrid mail provider"""
     @staticmethod
     def send_email(to_email, subject, text, substitutions=None):
         """Send an email to the recipient."""
+        mail = current_app.extensions['mail']
+        DOMAIN_NAME = current_app.config['DOMAIN_NAME']
+
         if current_app.config['TEST_MAIL'] or current_app.config['PRODUCTION']:
             from_email = 'no-reply@{domain_name}'.format(domain_name=DOMAIN_NAME)
             mail.send_email(
