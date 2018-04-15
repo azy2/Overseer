c532366288479d81cd6773802c988b6238500a6c
Yash Sharma <ysharma5@illinois.edu>
2018-04-15 06:32:23 +0000

    Fixed mail template syntax error

--- ovs/mail/templates.py
+++ ovs/mail/templates.py
@@ -1,7 +1,7 @@
 # pylint: disable=line-too-long
 """Contains the templates for sendgrid mailing"""
 TEMPLATES = {
-    'user_creation_email': '
+    'user_creation_email': 
         'Hi {first_name} {last_name},\n \
         Your {role} account was successfully created. Please click the link below<br>\n \
         to confirm your email address and activate your account:\n \
