e36a6d98b633244948baad3743a39090f93b88af
Ben Lambeth <lambethben@gmail.com>
2018-04-15 18:47:08 -0500

    Mock hashing functions to speed up tests

--- ovs/tests/unittests/base_test.py
+++ ovs/tests/unittests/base_test.py
@@ -8,8 +8,13 @@ from ovs import db
 from ovs.datagen import DataGen
 
 
-def new_generate_password_hash(self, password, rounds=None):
+def mock_generate_password_hash(self, password, rounds=None): # pylint: disable=unused-argument
+    """ Mocks password hashing during tests. """
     return password
+
+def mock_check_password_hash(self, pw_hash, password): # pylint: disable=unused-argument
+    """ Mocks a password compare during tests """
+    return pw_hash == password
 
 class OVSBaseTestCase(LiveServerTestCase):
     """
@@ -28,9 +33,13 @@ class OVSBaseTestCase(LiveServerTestCase):
         self.db = db
         DataGen.create_default_room()
         self.hash_patch = patch('flask_bcrypt.Bcrypt.generate_password_hash',
-                                new=new_generate_password_hash)
+                                new=mock_generate_password_hash)
         self.addCleanup(self.hash_patch.stop)
         self.hash_patch.start()
+        self.check_hash_patch = patch('flask_bcrypt.Bcrypt.check_password_hash',
+                                      new=mock_check_password_hash)
+        self.addCleanup(self.check_hash_patch.stop)
+        self.check_hash_patch.start()
 
     def tearDown(self):
         """
