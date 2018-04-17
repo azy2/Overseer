d9d24537239b6ec05b60bfbd7b197408e72a7511
David <buksa2@illinois.edu>
2018-04-16 20:35:12 -0500

    Profile delete no longer does direction computation, so I deleted some tests

--- ovs/tests/unittests/services/test_profile_service.py
+++ ovs/tests/unittests/services/test_profile_service.py
@@ -48,24 +48,6 @@ class TestProfileService(OVSBaseTestCase):
         self.assertEqual(profile.gender, Gender.FEMALE)
         self.assertIsNotNone(profile.picture_id)
 
-    def test_delete_profile(self):
-        """ Tests that profiles can be deleted """
-        expected = self.db.session.query(Profile).count() - 1
-
-        # check if deletion successful
-        self.assertTrue(ProfileService.delete_profile(self.test_user.id))
-
-        self.assertEqual(len(ProfileService.get_all_profiles()), expected)
-
-    def test_delete_profile_null(self):
-        """ Tests that nothing breaks when deleting a nonexistant profile """
-        expected = self.db.session.query(Profile).count()
-
-        # This id is NOT the resident
-        self.assertFalse(ProfileService.delete_profile(self.test_user.id + 1))
-
-        self.assertEqual(self.db.session.query(Profile).count(), expected)
-
     def test_get_all_profiles(self):
         """ Tests that get_all_profiles returns the correct number of profiles """
         expected = self.db.session.query(Profile).count()
