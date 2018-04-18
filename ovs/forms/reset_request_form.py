3d163f0bcffc2fa217ff07bce5d815cbbc9dfe0b
Ben Lambeth <lambethben@gmail.com>
2018-04-17 20:06:18 -0500

    Better forms and deleted windows newlines

--- ovs/forms/reset_request_form.py
+++ ovs/forms/reset_request_form.py
@@ -2,8 +2,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email
+from ovs.forms.validators import EmailRegistered
 
 
 class ResetRequestForm(FlaskForm):
     """ Form with data required to login."""
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email(), EmailRegistered(check=True)])
