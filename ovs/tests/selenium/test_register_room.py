e72f09cd5971b68e6ff047fffde4b5a8dc176505
AlexMackowiak <Alex@Mackowiaks.com>
2018-04-20 23:05:34 -0500

    Added 3 new tests. Two for cross_system and one for invalid room registration.

--- ovs/tests/selenium/test_register_room.py
+++ ovs/tests/selenium/test_register_room.py
@@ -33,8 +33,35 @@ class TestRegisterRoom(SeleniumBaseTestCase):
         wait = WebDriverWait(self.browser, 5)
         wait.until(EC.visibility_of_element_located((By.ID, 'notification-message')))
 
-        # Verify fields are empty and ready for new account registration
-        self.assertEqual(self.browser.find_element_by_id('room_number').get_attribute('value'), '')
-        self.assertEqual(self.browser.find_element_by_id('room_status').get_attribute('value'), '')
-        self.assertEqual(self.browser.find_element_by_id('room_type').get_attribute('value'), '')
-        self.assertEqual(self.browser.find_element_by_id('occupants').get_attribute('value'), '')
+        # Verify information added correctly, new rooms get added to the bottom of the table
+        # Format is [Room Number][Room Status][Room Type][Number of Occupants]
+        rooms_table = self.browser.find_element_by_class_name('table-responsive')
+        last_table_row = rooms_table.find_elements_by_tag_name('tr')[-1]
+        last_row_entries = last_table_row.find_elements_by_tag_name('td')
+        room_number = last_row_entries[0].find_element_by_class_name('form-control').get_attribute('value')
+        room_status = last_row_entries[1].find_element_by_class_name('form-control').get_attribute('value')
+        room_type = last_row_entries[2].find_element_by_class_name('form-control').get_attribute('value')
+        num_occupants = last_row_entries[3].text
+        self.assertEqual(room_number, '429D')
+        self.assertEqual(room_status, 'rented')
+        self.assertEqual(room_type, 'single')
+        self.assertEqual(num_occupants, '1')
+
+    def test_register_room_invalid(self):
+        """ Tests that rooms cannot be registered for residents that do not exist. """
+        self.browser.get(self.base_url)
+        self.assertIn('Overseer', self.browser.title)
+        super().login_default_admin()
+        
+        # Attempt to add a resident that doesn't exist to a room
+        register_rooms_link = self.browser.find_element_by_link_text('Rooms')
+        register_rooms_link.click()
+        self.set_text_field_by_id('room_number', '123')
+        self.set_text_field_by_id('room_status', 'Legitimate Status')
+        self.set_text_field_by_id('room_type', 'Yes')
+        self.set_text_field_by_id('occupants', 'real_email@gmail.com')
+        self.browser.find_element_by_id('register_room').click()
+
+        # Wait for failure notification
+        wait = WebDriverWait(self.browser, 5)
+        wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'invalid-feedback')))
