d8e1056ccd2c185d35346c01bde27e1edceccb88
AlexMackowiak <Alex@Mackowiaks.com>
2018-04-22 17:56:52 -0500

    Hotfix.

--- ovs/tests/selenium/test_cross_system.py
+++ ovs/tests/selenium/test_cross_system.py
@@ -1,9 +1,7 @@
 """ Tests functionality across routes. """
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
 from ovs.tests.selenium.selenium_base_test import SeleniumBaseTestCase
 
 class TestCrossSystem(SeleniumBaseTestCase):
